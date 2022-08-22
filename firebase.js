4c1d35b5351140b70a1fc6975a06e5cd2158bdb2
anikethromish <anikethromish@github.com>
2022-08-23 02:40:19 +0530

    added to do files. HTML,CSS, JAVASCRIPT

---
+++ firebase.js
@@ -0,0 +1,20 @@
+/* import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-analytics.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD3XNXaR_1y8gmPacsCKMN3109qIbhiJLo",
+    authDomain: "todo-list-7732e.firebaseapp.com",
+    projectId: "todo-list-7732e",
+    storageBucket: "todo-list-7732e.appspot.com",
+    messagingSenderId: "681210854580",
+    appId: "1:681210854580:web:30cf350d602346105cabe1",
+    measurementId: "G-6LCSHRXC5B"
+    };
+
+firebaseConfig.initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+
+ */
+
+
---
+++ plan.md
@@ -0,0 +1,11 @@
+create the boxes 
+create the banner background image
+create header
+create the input box 
+create the to-do elements
+set-up database
+save data to firebase
+create a functionallity to grab the data from the database
+create completed tasks with check mark 
+
+
